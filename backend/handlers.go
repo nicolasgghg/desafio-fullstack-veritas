4d14dc9a14bee18c4c628d9fc09f7c8f7a63b54d
Nicolas Gabriel de Souza Bernardino <34173655+nicolasgghg@users.noreply.github.com>
2026-08-12 13:13:03 -0300

    feat: add task listing endpoint

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,1 +1,17 @@
 package backend
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// In-memory task storage
+var tasks = make(map[int]Task)
+var nextID = 1
+
+// Task handlers
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(tasks)
+}
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,24 @@
 package backend
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	//tasks route handlers
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.Method {
+
+		case http.MethodGet:
+			getTasks(w, r)
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	// Start server on port 8080
+	http.ListenAndServe(":8080", nil)
+}
